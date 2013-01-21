a87d26bd60675303f4eae701034fc9875712df13
elizarov <elizarov@gmail.com>
2013-01-22 00:43:38 +0400

    Workaround for http://gcc.gnu.org/bugzilla/show_bug.cgi?id=34734

--- xprint.h
+++ xprint.h
@@ -13,6 +13,12 @@ void printOn_P(Print& out, PGM_P str);
 
 inline void print_P(PGM_P str) { printOn_P(Serial, str); }
 
+// Workaround for http://gcc.gnu.org/bugzilla/show_bug.cgi?id=34734 
+#ifdef PROGMEM 
+#undef PROGMEM 
+#define PROGMEM __attribute__((section(".progmem.data"))) 
+#endif
+
 #define printOn_C(out, str) { static const char _s[] PROGMEM = str; printOn_P(out, &_s[0]); }
 #define print_C(str)        { static const char _s[] PROGMEM = str; print_P(&_s[0]); }
 
